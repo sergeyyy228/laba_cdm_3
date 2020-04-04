29725e42299961c16d608e4aa9c8650fec7bddc1
Magnus07 <stanislav.pinchuk.02@gmail.com>
2020-04-04 17:30:05 +0300

    Add core functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -12,4 +12,97 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    if (!countBrackets(ui->lineEdit->text()))
+    {
+        QMessageBox msgBox;
+        msgBox.setText("Error");
+        msgBox.setInformativeText("Number of openning and closing brackets isn't equal!");
+        msgBox.exec();
+    }else
+    {
+        QStringList variables = findVariables(ui->lineEdit->text());
+        if (variables.length() > 4)
+        {
+            QMessageBox msgBox;
+            msgBox.setText("Error");
+            msgBox.setInformativeText("Too more variables.");
+            msgBox.exec();
+        }
+        else
+        {
+            buildTable(variables, ui->lineEdit->text());
+        }
+    }
+}
+
+
+bool MainWindow::countBrackets(QString data)
+{
+    if (data.count("(") == data.count(")"))
+        return true;
+    else
+        return false;
+}
+
+
+QStringList MainWindow::findVariables(QString str)
+{
+    QStringList list = str.split(QRegularExpression("\\W+"));
+    list.removeDuplicates();
+    list.sort();
+
+    return list;
+}
+
+
+QStringList MainWindow::findOperatons(QString str)
+{
+    QStringList list = str.split(QRegularExpression("\([&|+]{1,})"));
+    list.removeDuplicates();
+    list.sort();
+
+    return list;
+}
+
+
+void MainWindow::buildTable(QStringList variables, QString lineEdit)
+{
+
+    QStringList data = ui->lineEdit->text().split(QRegularExpression("\\W+"));
+    QStringList operations = lineEdit.split(QRegularExpression("\\w+"));
+    operations.removeAll("");
+
+    QStringList headers;
+    headers.append(variables);
+
+    for (int i = 0; i < operations.length();i++)
+    {
+        if (i!= 0)
+            headers.append(headers[headers.length()-1] + data[i]+operations[i]+data[i+1]);
+        else
+            headers.append(data[i]+operations[i]+data[i+1]);
+    }
+//    headers.append("f");
+    ui->tableWidget->setColumnCount(variables.length() + operations.length());
+    ui->tableWidget->setRowCount((int)pow(2,variables.length()));
+
+    ui->tableWidget->setHorizontalHeaderLabels(headers);
+//    ui->tableWidget->setVerticalHeaderLabels(inArr2);
+
+//    for (int i = 0; i < all;i++)
+//    {   // встановлюємо рядок і стовпець
+//        int row = i / (inArr1.length());
+//        int column = i % (inArr1.length());
+//        // встановлюємо значення
+//        QTableWidgetItem * item = new QTableWidgetItem(QString::number(compare(inArr1[column].toInt(),inArr2[row].toInt())));
+
+//        ui->tableWidget->setItem(row, column, item);
+//    }
+    // змінюємо розміри таблиці
+    ui->tableWidget->resizeRowsToContents();
+    ui->tableWidget->resizeColumnsToContents();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,9 @@
-#ifndef MAINWINDOW_H
+﻿#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
+#include <math.h>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    bool countBrackets(QString data);
+    QStringList findVariables(QString data);
+
+    QStringList findOperatons(QString str);
+    void buildTable(QStringList variables, QString lineEdit);
 };
 
 #endif // MAINWINDOW_H
