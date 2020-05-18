4becb0b9de5c15ef7aa6f3a5726d7169a2362826
Sergey <sezonchik040901@gmail.com>
2020-05-18 11:45:16 +0300

    fix implication

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,4 @@
-﻿#include "mainwindow.h"
+#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -30,40 +30,40 @@ void MainWindow::on_pushButton_clicked()
 // функція для визначення результату булевих функцій
 int MainWindow::calculate(int x, int y, QString action)
 {
-    if (action == '&')
+    if (action == '&')//коньюкция
     {
         if ( x == 1 && y == 1)
             return 1;
         else
             return 0;
     }
-    else if(action == '|')
+    else if(action == '|')//штрих шеффера
     {
         if (x == 1 && y == 1)
             return 0;
         else
             return 1;
     }
-    else if (action == '*')
+    else if (action == '*')//дизьюнкция
     {
         if (x == 0 && y == 0)
             return 0;
         else
             return 1;
     }
-    else if (action == '+'){
+    else if (action == '+'){//прибавление за модулем
         if(x!=y){
             return 1;
         }else return 0;
     }
-    else if (action == '#'){
+    else if (action == '#'){//стрелка пирса
         if (x == 0 && y == 0)
             return 1;
         else
             return 0;
     }
-    else if (action == '@'){
-        if(x==0&&y==1){
+    else if (action == '@'){//импликация
+        if(x==1&&y==0){
             return 0;
         }else return 1;
     }
